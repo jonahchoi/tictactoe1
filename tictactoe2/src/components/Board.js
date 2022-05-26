24a5b1a9b3345c4c2b56ea3f1fa365f59e685349
jonahchoi <cjonah227@gmail.com>
2022-05-26 02:20:11 -0700

    Adjusted for readability

--- tictactoe2/src/components/Board.js
+++ tictactoe2/src/components/Board.js
@@ -9,21 +9,66 @@ const columnClassEnd = 'd-flex flex-column justify-content-center border-2 borde
 const Board = ({handleClick, showModal, boardPlacement}) => {
   
   return (
-    <Container className="text-center" style={{height: "600px", width: "600px"}}>
+    <Container className="text-center" style={{height: "600px", width: "600px", fontSize: "100px"}}>
       <Row style={{height: '200px'}}>
-        <Col onClick={()=> handleClick(0)} className={columnClassEndBottom}>{boardPlacement[0]}</Col>
-        <Col onClick={()=> handleClick(1)} className={columnClassEndBottom}>{boardPlacement[1]}</Col>
-        <Col onClick={()=> handleClick(2)} className={columnClassBottom}>{boardPlacement[2]}</Col>
+        <Col 
+          onClick={()=> handleClick(0)} 
+          className={columnClassEndBottom}
+        >
+          {boardPlacement[0]}
+        </Col>
+        <Col 
+        onClick={()=> handleClick(1)} 
+        className={columnClassEndBottom}
+        >
+          {boardPlacement[1]}
+        </Col>
+        <Col 
+        onClick={()=> handleClick(2)} 
+        className={columnClassBottom}
+        >
+          {boardPlacement[2]}
+        </Col>
       </Row>
       <Row style={{height: '200px'}}>
-        <Col onClick={()=> handleClick(3)} className={columnClassEndBottom}>{boardPlacement[3]}</Col>
-        <Col onClick={()=> handleClick(4)} className={columnClassEndBottom}>{boardPlacement[4]}</Col>
-        <Col onClick={()=> handleClick(5)} className={columnClassBottom}>{boardPlacement[5]}</Col>
+        <Col 
+        onClick={()=> handleClick(3)} 
+        className={columnClassEndBottom}
+        >
+          {boardPlacement[3]}
+        </Col>
+        <Col 
+        onClick={()=> handleClick(4)} 
+        className={columnClassEndBottom}
+        >
+          {boardPlacement[4]}
+        </Col>
+        <Col 
+        onClick={()=> handleClick(5)} 
+        className={columnClassBottom}
+        >
+          {boardPlacement[5]}
+        </Col>
       </Row>
       <Row style={{height: '200px'}}>
-        <Col onClick={()=> handleClick(6)} className={columnClassEnd}>{boardPlacement[6]}</Col>
-        <Col onClick={()=> handleClick(7)} className={columnClassEnd}>{boardPlacement[7]}</Col>
-        <Col onClick={()=> handleClick(8)} className={columnClass}>{boardPlacement[8]}</Col>
+        <Col 
+        onClick={()=> handleClick(6)} 
+        className={columnClassEnd}
+        >
+          {boardPlacement[6]}
+        </Col>
+        <Col 
+        onClick={()=> handleClick(7)} 
+        className={columnClassEnd}
+        >
+          {boardPlacement[7]}
+        </Col>
+        <Col 
+        onClick={()=> handleClick(8)} 
+        className={columnClass}
+        >
+          {boardPlacement[8]}
+        </Col>
       </Row>
     </Container>
     
