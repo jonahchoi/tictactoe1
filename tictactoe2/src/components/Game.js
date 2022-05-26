02c3e9e39b07bdf13a4b3105c432128532abb410
jonahchoi <cjonah227@gmail.com>
2022-05-25 19:11:22 -0700

    test commit

--- tictactoe2/src/components/Game.js
+++ tictactoe2/src/components/Game.js
@@ -1,14 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Board from './Board'
 import GameEndModal from './GameEndModal'
 import PlayerDisplay from './PlayerDisplay'
 
 const Game = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const hideModal = () => {
+    setShowModal(false);
+  }
   return (
     <>
       <Board />
       <PlayerDisplay />
-      <GameEndModal />
+      <GameEndModal show={showModal} onHide={hideModal} />
     </>
   )
 }
